a8d7d6a394a2d8dbb19145dc4ee8f27c2e1e0773
mugambi645 <mugambi645@gmail.com>
2021-10-29 11:05:46 +0300

    enabled configuration class settings in our app

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from .config import DevConfig
 #initializing the application
 app = Flask(__name__)
+#setting up configuration
+app.config.from_object(DevConfig)
 from app import views
